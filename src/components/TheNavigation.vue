aa671b4b922e40fd17896cd0ecadaec31089adfe
Liana Bisuna <liana@thinkbitsolutions.com>
2023-03-19 19:55:37 +0800

    Add dark mode - Add package @vueuse/core

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,10 +1,17 @@
 <script lang="ts" setup>
+import { useDark, useToggle } from '@vueuse/core'
 
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
 <template>
-  <aside>
-    <h1>Navigation</h1>
+  <aside class="flex flex-col overflow-hidden w-[300px] h-screen border-r border-gray-300 dark:border-gray-600 justify-between">
+    <ul>
+      <li>
+        <button @click="toggleDark()">Toggle Theme</button>
+      </li>
+    </ul>
   </aside>
 </template>
 
--- src/layouts/TheLayoutDashboard.vue
+++ src/layouts/TheLayoutDashboard.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
-import Header from '../components/TheHeader.vue';
+// import Header from '../components/TheHeader.vue';
 import Navigation from '../components/TheNavigation.vue';
 </script>
 
 <template>
-  <div>
-    <Header />
-    <main>
+  <div class="flex flex-col bg-gray-50 dark:bg-gray-700 text-gray-800 dark:text-gray-200 h-screen overflow-hidden select-none">
+    <!-- <Header /> -->
+    <main class="flex">
       <Navigation/>
       <div>
         <slot></slot>
