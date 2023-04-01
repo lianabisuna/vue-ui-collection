25886bfb01f2d82e178fc60030cbbabb99866eb9
Liana Bisuna <liana@thinkbitsolutions.com>
2023-04-01 19:39:07 +0800

    Add scrollbar class

--- src/App.vue
+++ src/App.vue
@@ -8,4 +8,26 @@ import LayoutHandler from './layouts/TheLayoutHandler.vue';
   </LayoutHandler>
 </template>
 
-<style scoped></style>
+<style lang="scss">
+.scrollbar {
+  &::-webkit-scrollbar {
+    -webkit-appearance: none;
+  }
+  &::-webkit-scrollbar:vertical {
+    width: 10px;
+  }
+  &::-webkit-scrollbar:horizontal {
+    height: 10px;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #e5e7eb;
+    border-radius: 0.3rem;
+    border: 2px solid #ffffff;
+  }
+  &::-webkit-scrollbar-track {
+    border-top-right-radius: 0.3rem;
+    border-bottom-right-radius: 0.3rem;
+    background-color: #ffffff;
+  }
+}
+</style>
