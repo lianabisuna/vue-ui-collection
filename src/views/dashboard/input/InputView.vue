1ab25d797d5f98164347aae9f20d4b4ab04b4917
Liana Bisuna <liana@thinkbitsolutions.com>
2023-04-03 19:22:52 +0800

    Update input component cards

--- src/views/dashboard/input/InputView.vue
+++ src/views/dashboard/input/InputView.vue
@@ -3,19 +3,19 @@ import { AppFormInput } from '@/components/app';
 </script>
 
 <template>
-  <div class="grid grid-cols-1 gap-3 p-3 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
-    <div class="flex items-center justify-center p-3 bg-gray-100 border border-gray-300 aspect-4/3">
-      <AppFormInput placeholder="Default"></AppFormInput>
+  <div class="grid grid-cols-1 gap-3 p-3">
+    <div class="flex items-center justify-center p-3 bg-gray-100 border border-gray-300 aspect-[4/1]">
+      <AppFormInput placeholder="Default" class="xs:11/12 md:w-8/12 xl:w-5/12"></AppFormInput>
     </div>
-    <div class="flex items-center justify-center p-3 bg-gray-100 border border-gray-300 aspect-4/3">
-      <AppFormInput>
+    <div class="flex items-center justify-center p-3 bg-gray-100 border border-gray-300 aspect-[4/1]">
+      <AppFormInput class="xs:11/12 md:w-8/12 xl:w-5/12">
         <template #append>
           <span class="text-gray-500">@example.com</span>
         </template>
       </AppFormInput>
     </div>
-    <div class="flex items-center justify-center p-3 bg-gray-100 border border-gray-300 aspect-4/3">
-      <AppFormInput disabled placeholder="Disabled"></AppFormInput>
+    <div class="flex items-center justify-center p-3 bg-gray-100 border border-gray-300 aspect-[4/1]">
+      <AppFormInput disabled placeholder="Disabled" class="xs:11/12 md:w-8/12 xl:w-5/12"></AppFormInput>
     </div>
   </div>
 </template>
