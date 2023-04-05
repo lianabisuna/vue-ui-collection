b23022bada8400464ddbd851a9e5fe8d32cd3591
Liana Bisuna <liana@thinkbitsolutions.com>
2023-04-05 20:01:14 +0800

    Add tooltip component

--- src/components/app/AppTooltip.vue
+++ src/components/app/AppTooltip.vue
@@ -9,14 +9,26 @@ export type TailwindColor = `${string}-${number}`|'black'|'white'
 const props = defineProps({
   position: { type: String as PropType<TooltipPosition>, default: 'top' },
   color: { type: String as PropType<TailwindColor>, default: 'gray-500' },
+  hideArrow: { type: Boolean as PropType<boolean>, default: false },
 })
 
 const positionClass = computed(() => {
   switch (props.position) {
-    case 'top': return 'mb-3'
-    case 'right': return 'ml-3'
-    case 'bottom': return 'mt-3'
-    case 'left': return 'mr-3'
+    case 'top': return 'mb-3 bottom-full left-1/2 -translate-x-1/2'
+    case 'right': return 'ml-3 left-full top-1/2 -translate-y-1/2'
+    case 'bottom': return 'mt-3 top-full left-1/2 -translate-x-1/2'
+    case 'left': return 'mr-3 right-full top-1/2 -translate-y-1/2'
+  }
+})
+
+const arrowClass = computed(() => {
+  if (!props.hideArrow) {
+    switch (props.position) {
+      case 'top': return ''
+      case 'right': return ''
+      case 'bottom': return ''
+      case 'left': return ''
+    }
   }
 })
 </script>
@@ -27,7 +39,7 @@ const positionClass = computed(() => {
     <div
       :class="[
         `bg-${color}`,
-        positionClass
+        positionClass, arrowClass
       ]"
       class="absolute whitespace-nowrap rounded py-2 px-3 font-semibold text-white opacity-0 group-hover:opacity-100"
     >
