cfb9bb5313477e3be0a28aa19ef7353523d715eb
Liana Bisuna <liana@thinkbitsolutions.com>
2023-04-05 10:41:39 +0800

    Update check svg

--- src/components/app/AppFormCheckbox.vue
+++ src/components/app/AppFormCheckbox.vue
@@ -41,7 +41,7 @@ const updateModelValue = (event: Event) => {
         >
           <span class="h-3 w-3">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-              <path d="M9 22l-10-10.598 2.798-2.859 7.149 7.473 13.144-14.016 2.909 2.806z"/>
+              <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/>
             </svg>
           </span>
         </div>
